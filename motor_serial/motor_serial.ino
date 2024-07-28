6dbac2a3b2b0e23a85dfa79de2a5214447285a43
Alex <alexkwasinski@gmail.com>
2024-07-27 22:28:48 -0400

    updated arduino code with pins attached to all pwm and dir

--- motor_serial/motor_serial.ino
+++ motor_serial/motor_serial.ino
@@ -1,59 +1,114 @@
-#define PWM1 10
-#define DIR1 13
-#define PWM2 6
-#define DIR2 9
+#define PWM1Front 3 //all 1's are on right side of robot
+#define PWM1Middle 5
+#define PWM1Back 6
+#define DIR1Front 52
+#define DIR1Middle 50
+#define DIR1Back 48
+#define PWM2Front 9 //all 2's are on the left side or robot
+#define PWM2Middle 10
+#define PWM2Back 11
+#define DIR2Front 53
+#define DIR2Middle 51
+#define DIR2Back 49
 
-double pwmSpeed = 50;
+int pwmSpeed = 50;
 
 void setup() 
 {
   Serial.begin(115200);
   Serial.println("in setup");
 
-  pinMode(DIR1, OUTPUT);
-  pinMode(DIR2, OUTPUT);
-  pinMode(PWM1, OUTPUT);
-  pinMode(PWM2,   OUTPUT);
+  pinMode(DIR1Front, OUTPUT);
+  pinMode(DIR1Middle, OUTPUT);
+  pinMode(DIR1Back, OUTPUT);
+  pinMode(DIR2Front, OUTPUT);
+  pinMode(DIR2Middle, OUTPUT);
+  pinMode(DIR2Back, OUTPUT);
+  pinMode(PWM1Front, OUTPUT);
+  pinMode(PWM1Middle, OUTPUT);
+  pinMode(PWM1Back, OUTPUT);
+  pinMode(PWM2Front,   OUTPUT);
+  pinMode(PWM2Middle,   OUTPUT);
+  pinMode(PWM2Back,   OUTPUT);
+//
+//  pwmSpeed = Serial.parseInt();
 }
 
 void foward() {
-  pwmSpeed = 50;
-  digitalWrite(DIR1, HIGH);
-  digitalWrite(DIR2, HIGH);
-  digitalWrite(PWM1, pwmSpeed);
-  digitalWrite(PWM2, pwmSpeed);
+  digitalWrite(DIR1Front, HIGH);
+  digitalWrite(DIR1Middle, HIGH);
+  digitalWrite(DIR1Back, HIGH);
+  digitalWrite(DIR2Front, HIGH);
+  digitalWrite(DIR2Middle, HIGH);
+  digitalWrite(DIR2Back, HIGH);
+  analogWrite(PWM1Front, pwmSpeed);
+  analogWrite(PWM1Middle, pwmSpeed);
+  analogWrite(PWM1Back, pwmSpeed);
+  analogWrite(PWM2Front, pwmSpeed);
+  analogWrite(PWM2Middle, pwmSpeed);
+  analogWrite(PWM2Back, pwmSpeed);
 }
 
 void reverse() {
-  pwmSpeed = 50;
-  digitalWrite(DIR1, LOW);
-  digitalWrite(DIR2, LOW);
-  digitalWrite(PWM1, pwmSpeed);
-  digitalWrite(PWM2, pwmSpeed);
+  digitalWrite(DIR1Front, LOW);
+  digitalWrite(DIR1Middle, LOW);
+  digitalWrite(DIR1Back, LOW);
+  digitalWrite(DIR2Front, LOW);
+  digitalWrite(DIR2Middle, LOW);
+  digitalWrite(DIR2Back, LOW);
+  analogWrite(PWM1Front, pwmSpeed);
+  analogWrite(PWM1Middle, pwmSpeed);
+  analogWrite(PWM1Back, pwmSpeed);
+  analogWrite(PWM2Front, pwmSpeed);
+  analogWrite(PWM2Middle, pwmSpeed);
+  analogWrite(PWM2Back, pwmSpeed);
 }
 
 void left() {
-  pwmSpeed = 50;
-  digitalWrite(DIR1, HIGH);
-  digitalWrite(DIR2, HIGH);
-  digitalWrite(PWM1, pwmSpeed);
-  digitalWrite(PWM2, 0);
+  digitalWrite(DIR1Front, HIGH);
+  digitalWrite(DIR1Middle, HIGH);
+  digitalWrite(DIR1Back, HIGH);
+  digitalWrite(DIR2Front, HIGH);
+  digitalWrite(DIR2Middle, HIGH);
+  digitalWrite(DIR2Back, HIGH);
+  analogWrite(PWM1Front, pwmSpeed);
+  analogWrite(PWM1Middle, pwmSpeed);
+  analogWrite(PWM1Back, pwmSpeed);
+  analogWrite(PWM2Front, 0);
+  analogWrite(PWM2Middle, 0);
+  analogWrite(PWM2Back, 0);
 }
 
 void right() {
-  pwmSpeed = 50;
-  digitalWrite(DIR1, HIGH);
-  digitalWrite(DIR2, HIGH);
-  digitalWrite(PWM1, 0);
-  digitalWrite(PWM2, pwmSpeed);
+  digitalWrite(DIR1Front, HIGH);
+  digitalWrite(DIR1Middle, HIGH);
+  digitalWrite(DIR1Back, HIGH);
+  digitalWrite(DIR2Front, HIGH);
+  digitalWrite(DIR2Middle, HIGH);
+  digitalWrite(DIR2Back, HIGH);
+  analogWrite(PWM1Front, 0);
+  analogWrite(PWM1Middle, 0);
+  analogWrite(PWM1Back, 0);
+  analogWrite(PWM2Front, pwmSpeed);
+  analogWrite(PWM2Middle, pwmSpeed);
+  analogWrite(PWM2Back, pwmSpeed);
 }
 
-void boost() {
-  while (pwmSpeed <= 200){
-    digitalWrite(DIR1, HIGH);
-    digitalWrite(DIR2, HIGH);
-    digitalWrite(PWM1, pwmSpeed);
-    digitalWrite(PWM2, pwmSpeed);
+void boost(int pwmSpeed, int newSpeed) {
+  newSpeed = 150;
+  while (pwmSpeed <= newSpeed){
+    digitalWrite(DIR1Front, HIGH);
+    digitalWrite(DIR1Middle, HIGH);
+    digitalWrite(DIR1Back, HIGH);
+    digitalWrite(DIR2Front, HIGH);
+    digitalWrite(DIR2Middle, HIGH);
+    digitalWrite(DIR2Back, HIGH);
+    analogWrite(PWM1Front, pwmSpeed);
+    analogWrite(PWM1Middle, pwmSpeed);
+    analogWrite(PWM1Back, pwmSpeed);
+    analogWrite(PWM2Front, pwmSpeed);
+    analogWrite(PWM2Middle, pwmSpeed);
+    analogWrite(PWM2Back, pwmSpeed);
     pwmSpeed += 1;
     Serial.println(pwmSpeed);
     delay(10);
@@ -62,11 +117,19 @@ void boost() {
 
 void stopAll() {
   while (pwmSpeed >= 0){
-    digitalWrite(DIR1, HIGH);
-    digitalWrite(DIR2, HIGH);
-    digitalWrite(PWM1, pwmSpeed);
-    digitalWrite(PWM2, pwmSpeed);
-    pwmSpeed -= 1.00;
+    digitalWrite(DIR1Front, HIGH);
+    digitalWrite(DIR1Middle, HIGH);
+    digitalWrite(DIR1Back, HIGH);
+    digitalWrite(DIR2Front, HIGH);
+    digitalWrite(DIR2Middle, HIGH);
+    digitalWrite(DIR2Back, HIGH);
+    analogWrite(PWM1Front, pwmSpeed);
+    analogWrite(PWM1Middle, pwmSpeed);
+    analogWrite(PWM1Back, pwmSpeed);
+    analogWrite(PWM2Front, pwmSpeed);
+    analogWrite(PWM2Middle, pwmSpeed);
+    analogWrite(PWM2Back, pwmSpeed);
+    pwmSpeed -= 1;
     Serial.println(pwmSpeed);
 //    delay(10);
   }
@@ -99,7 +162,8 @@ void loop() {
     }
     else if (input.equals("5")){
       Serial.println("Boost!");
-      boost();
+      //int pwmSpeed = Serial.parseInt();
+      boost(pwmSpeed, Serial.parseInt());
     }
     else if (input.equals("6")){
       Serial.println("STOP!");
