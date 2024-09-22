d201dbeea89addd1e97824cd28d303b4e929ef87
Alex <alexkwasinski@gmail.com>
2024-09-22 12:15:38 -0400

    added folder within motor_serial to test encoder code

---
+++ motor_serial/motor_encoders/motor_encoder_test.ino/motor_encoder_test.ino.ino
@@ -0,0 +1,96 @@
+//Define pin numbers for motor
+#define DIR1 52
+#define PWM1 3
+
+//Define pin numbmers for encoder
+#define encoderPinA 22
+#define encoderPinB 23
+
+//Variable for encoder counts
+volatile long encoderCount = 0;
+
+//Variables for PID Control
+long previousTime = 0;
+float ePrevious = 0;
+float eIntegral = 0;
+
+void setup() { 
+  Serial.begin(9600);
+
+  //Set pin modes 
+  pinMode (DIR1, OUTPUT); 
+  pinMode (PWM1, OUTPUT); 
+  pinMode (encoderPinA, INPUT); 
+  pinMode (encoderPinB, INPUT);
+  
+  //Interrupt for encoder
+  attachInterrupt(digitalPinToInterrupt(encoderPinA), handleEncoder, RISING);
+}
+void loop() {
+  //Setpoint
+  int target = 1000;
+  
+  //PID gains and computation
+  float kp = 5.0;
+  float kd = 0.0;
+  float ki = 0.0;
+  float u = pidController(target, kp, kd, ki);
+  Serial.println(u);
+  //Control motor
+  moveMotor(DIR1, PWM1, u);
+
+
+  //Print statements for debugging
+  Serial.print(target);
+  Serial.print(", ");
+  Serial.println(encoderCount);
+}
+
+//Function called during interrupts
+void handleEncoder() {
+  if (digitalRead(encoderPinA) > digitalRead(encoderPinB)) {
+    encoderCount++;
+  }
+  else{
+    encoderCount--;
+  }
+}
+
+//Function to move motor
+void moveMotor(int dirPin, int pwmPin, float u){
+  //Maximum motor speed
+  float pwmspeed = fabs(u);
+  if (pwmspeed > 255) {
+    pwmspeed = 255;
+  }
+  
+  //set the direction
+  int motorDirection = 1;
+  if (u < 0) {
+    motorDirection = 0;
+  }
+  
+  //Control the motor
+  digitalWrite(dirPin, motorDirection);
+  analogWrite(pwmPin, pwmspeed);
+}
+
+float pidController (int target, float kp, float kd, float ki) {
+  //Measure the time elapsed since the last iteration
+  long currentTime = micros(); 
+  float deltaT = ((float)(currentTime - previousTime)) / 1.0E6;
+  
+  //Compute the error, derivative, and integral 
+  int e = encoderCount - target;
+  float eDerivative = (e - ePrevious) / deltaT;
+  eIntegral = eIntegral + e * deltaT;
+  
+  //Compute the PID control signal 
+  float u = (kp * e) + (kd * eDerivative) + (ki * eIntegral);
+  
+  //Update variables for the next iteration 
+  previousTime = currentTime; 
+  ePrevious = e;
+  
+  return u;
+}
